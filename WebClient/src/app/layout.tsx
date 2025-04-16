41c01aee0444a008cd588016b889226d75715f50
Chris1320 <me@chrisfromnowhere.me>
2025-04-16 18:39:33 +0800

    feat(web-client): enhance Ant Design and NextJS integration
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

--- WebClient/src/app/layout.tsx
+++ WebClient/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Program } from "@/lib/info";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
